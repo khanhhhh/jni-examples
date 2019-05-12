320ce5552a8a7397fa765d4aded77d1ee1338bcf
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2019-05-12 21:39:01 +0800

    modified:   Makefile 	modified:   main.java

--- MatrixMultiplicationJNI/main.java
+++ MatrixMultiplicationJNI/main.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.io.PrintWriter;
 public class main {
 	private static long Jtest(int size, double[] s1, double[] s2) {
 		Matrix m1 = new Matrix(size, size, s1);
@@ -7,7 +8,7 @@ public class main {
 		long tic = System.nanoTime();
 		Matrix m3 = Matrix.multiplication(m1, m2);
 		long toc = System.nanoTime();
-		return (toc-tic)/1000000;
+		return toc-tic;
 	}
 	private static long Ctest(int size, double[] s1, double[] s2) {
 		CMatrix m1 = new CMatrix(size, size, s1);
@@ -16,21 +17,24 @@ public class main {
 		long tic = System.nanoTime();
 		CMatrix m3 = CMatrix.multiplication(m1, m2);
 		long toc = System.nanoTime();
-		return (toc-tic)/1000000;
+		return toc-tic;
 	}
-	public static void main(String[] args) {
-		int size = 500;
-		Random r = new Random(1234);
+	public static double[] test(Random r, int size) {
 		double[] s1 = new double[size*size];
 		double[] s2 = new double[size*size];
 		for (int i=0; i<size*size; i++) {
 			s1[i] = r.nextDouble();
 			s2[i] = r.nextDouble();
 		}
-		long jtime = Jtest(size, s1, s2);
-		long ctime = Ctest(size, s1, s2);
-
-		System.out.println("Jtest: " + jtime + " ms");
-		System.out.println("Ctest: " + ctime + " ms");
+		double jtime = Jtest(size, s1, s2);
+		double ctime = Ctest(size, s1, s2);
+		return new double[]{jtime/1000000, ctime/1000000};
+	}
+	public static void main(String[] args) {
+		Random r = new Random(1234);
+		for (int size=1; size <= 1024; size += 50) {
+			double[] res = test(r, size);
+			System.out.println(size + "," + res[0] + "," + res[1]);
+		}
 	}
 }
